1559db3336a2cfe5593b165839b563348385a17b
Philip Curley <philip@interactiveknowledge.com>
2019-12-09 13:53:24 -0500

    Add autoupdater

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron')
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
+const server = 'https://pgc-electron-app.heroku.com'
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+if (process.env.ENVIRONMENT === 'production') {
+  autoUpdater.setFeedURL(feed)
+
+  setInterval(() => {
+    console.log('Checking...')
+    autoUpdater.checkForUpdates()
+  }, 60000)
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
+}
 
 app.on('ready', () => {
   const window = new BrowserWindow({
