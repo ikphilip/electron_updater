b59d4366df797d3554b9a3b5bf85d7f538bb8fd9
Philip Curley <philip@interactiveknowledge.com>
2019-12-11 10:32:33 -0500

    Improve build options

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const { app, BrowserWindow } = require('electron')
-const { autoUpdater } = require('electron-updater')
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
 const log = require('electron-log')
+const server = 'https://pgc-electron-app.heroku.com'
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
 
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+log.info('App is starting.')
 
 app.on('ready', () => {
   log.info('App is ready!')
 
-  autoUpdater.checkForUpdatesAndNotify()
+  if (process.env.ENV === 'production') {
+    log.info('Check for updates.')
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+      const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+      }
+
+      dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+      })
+    })
+
+    autoUpdater.on('error', message => {
+      log.error('There was a problem updating the application')
+      log.error(message)
+    })
+
+    autoUpdater.setFeedURL(feed)
+
+    setInterval(() => {
+      log.info('Checking...')
+      autoUpdater.checkForUpdates()
+    }, 5000)
+  } else {
+    log.info('Will not check for updates.')
+  }
 
   const window = new BrowserWindow({
     fullscreen: false,
@@ -32,7 +61,7 @@ app.on('ready', () => {
 
   window.removeMenu()
 
-  window.loadURL('https://drupal.org')
+  window.loadURL('http://drupal.org')
 
   window.once('ready-to-show', () => {
     window.show()
