a59d5b9482399418fcbf22dd5d496d335c9ef55c
Philip Curley <philip@interactiveknowledge.com>
2019-12-11 10:08:36 -0500

    Try electron-updater

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
-const server = 'https://pgc-electron-app.heroku.com'
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
 
-if (process.env.ENVIRONMENT === 'production') {
-  autoUpdater.setFeedURL(feed)
-
-  setInterval(() => {
-    console.log('Checking...')
-    autoUpdater.checkForUpdates()
-  }, 60000)
-
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-    }
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      if (returnValue.response === 0) autoUpdater.quitAndInstall()
-    })
-  })
-
-  autoUpdater.on('error', message => {
-    console.error('There was a problem updating the application')
-    console.error(message)
-  })
-}
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 app.on('ready', () => {
+  log.info('App is ready!')
+
+  autoUpdater.checkForUpdatesAndNotify()
+
   const window = new BrowserWindow({
     fullscreen: false,
     height: 600,
@@ -45,7 +25,7 @@ app.on('ready', () => {
   const { webContents } = window
 
   webContents.on('did-finish-load', () => {
-    webContents.setZoomFactor(1)
+    webContents.zoomFactor = 1
     webContents.setVisualZoomLevelLimits(1, 1)
     webContents.setLayoutZoomLevelLimits(0, 0)
   })
